e4183c366d8caae4740db3f04e8a42d205932f31
Lucas Dohmen <lucas@dohmen.io>
2016-12-04 18:32:37 +0100

    Elm formatted

--- Cortex.elm
+++ Cortex.elm
@@ -7,45 +7,60 @@ import Keyboard
 
 -- MODEL
 
+
 type alias Model =
-    { buffer: List String -- Maybe (List List Char) ?
-    , cursorPosition: CursorPosition
+    { buffer : List String
+    , cursorPosition : CursorPosition
     }
 
 
 type alias CursorPosition =
-    { y: Int
+    { y : Int
     }
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-    ({ buffer = [ "" ], cursorPosition = { y = 0 } }, Cmd.none)
+    ( { buffer = [ "" ], cursorPosition = { y = 0 } }, Cmd.none )
+
 
 
 -- UPDATE
 
-type Msg = Presses Char.KeyCode
+
+type Msg
+    = Presses Char.KeyCode
 
 
 insertAtLine : Int -> Int -> String -> List String -> List String
 insertAtLine targetLine i newString list =
     if targetLine == i then
-       case list of
-           [] -> []
-           [x] -> [(String.append x newString)]
-           (x::xs) -> (String.append x newString) :: (insertAtLine targetLine (i+1) newString xs)
+        case list of
+            [] ->
+                []
+
+            [ x ] ->
+                [ (String.append x newString) ]
+
+            x :: xs ->
+                (String.append x newString) :: (insertAtLine targetLine (i + 1) newString xs)
     else
-       case list of
-           [] -> []
-           [x] -> [x]
-           (x::xs) -> x :: (insertAtLine targetLine (i+1) newString xs)
+        case list of
+            [] ->
+                []
+
+            [ x ] ->
+                [ x ]
+
+            x :: xs ->
+                x :: (insertAtLine targetLine (i + 1) newString xs)
 
 
 insertNewline : Model -> Model
 insertNewline model =
     let
-        newBuffer = List.concat [model.buffer, [ "" ]]
+        newBuffer =
+            List.concat [ model.buffer, [ "" ] ]
     in
         { model | buffer = newBuffer, cursorPosition = { y = model.cursorPosition.y + 1 } }
 
@@ -53,31 +68,38 @@ insertNewline model =
 insertChar : Char -> Model -> Model
 insertChar char model =
     let
-        newBuffer = insertAtLine model.cursorPosition.y 0 (String.fromChar char) model.buffer
+        newBuffer =
+            insertAtLine model.cursorPosition.y 0 (String.fromChar char) model.buffer
     in
         { model | buffer = newBuffer }
 
 
-update: Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Presses code ->
             case code of
                 -- Enter
-                13 -> (insertNewline model, Cmd.none)
+                13 ->
+                    ( insertNewline model, Cmd.none )
 
                 -- Other Chars
-                _ -> (insertChar (fromCode code) model, Cmd.none)
+                _ ->
+                    ( insertChar (fromCode code) model, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
 
-subscriptions: Model -> Sub Msg
+
+subscriptions : Model -> Sub Msg
 subscriptions model =
     Keyboard.presses (\code -> Presses code)
 
 
+
 -- VIEW
+
 
 displayBuffer : List String -> List (Html msg)
 displayBuffer buffer =
@@ -92,12 +114,14 @@ displayBuffer buffer =
 view : Model -> Html Msg
 view model =
     div []
-    [ main_ [] (displayBuffer model.buffer)
-    , aside [] [ text (toString model) ]
-    ]
+        [ main_ [] (displayBuffer model.buffer)
+        , aside [] [ text (toString model) ]
+        ]
+
 
 
 -- MAIN
+
 
 main : Program Never Model Msg
 main =
